6655b5976cded08dc3767cc27a38cd095b8db12c
Anthony Venturini <anthonydventurini@gmail.com>
2019-03-20 15:04:18 -0700

    Adds CRUD operations to user and post routers

--- data/routers/user-router.js
+++ data/routers/user-router.js
@@ -1,14 +1,80 @@
 const express = require('express');
-const postData = require('../helpers/postDb');
+const userData = require('../helpers/userDb');
 
 const router = express.Router();
 
 // get
+router.get('/', async (req,res)=>{
+    try{
+        const users = await userData.find(req.query);
+        res.status(200).json(users)
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({message: "Error retrieving users"})
+    }
+})
 // getById
+
+router.get('/:id', async (req, res)=>{
+    
+    try{
+        const user = await userData.findById(req.params.id);
+        if(user){
+            res.status(200).json(user)
+        }
+        else{
+            res.status(404).json({message: "Could not find user"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error retrieving user"})
+    }
+})
 // post
+router.post('/', async (req, res)=> {
+    try{    
+        const user = await userData.insert(req.body);
+        res.status(201).json(user)
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error adding the user"})
+    }
+})
 // update
+router.put('/:id', async (req, res)=> {
+    try{
+        const user = await userData.update(req.params.id, req.body);
+        if(user){
+            res.status(202).json(user)
+        }
+        else {
+            res.status(404).json({message: "Could not find user"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error updating user"})
+    }
+})
 // delete
-// postByUserId
+router.delete('/:id', async (req, res)=> {
+    try{
+        const count = await userData.remove(req.params.id);
+        if(count > 0){
+            res.status(200).json({message:"user deleted successfully"})
+        }
+        else {
+            res.status(404).json({message: "user could not be found"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error deleting user"})
+    }
+})
 
 
 module.exports = router;
